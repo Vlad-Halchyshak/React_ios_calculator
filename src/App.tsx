759a028d0e89061d81ab074cdf47188d5b6b5967
Vlad <vladhalchyshak@gmail.com>
2020-11-13 17:56:52 +0200

    switch mode added

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import './App.scss'
 import { DateTime } from './components/date'
 import {Main} from './components/main'
+import { Switch } from 'antd';
 
 const  App:React.FC = () => {
+  const [color, setColor] = React.useState(true)
+  
   
 return (
-    <div className="container">
-      <div className="top_part">
-        <DateTime />
-      </div>
+  <div className={ color ? "container" : "container2"}>
+    <div className="top_part">
+      <DateTime />
+      <Switch onClick={() => setColor(!color)} size="small" style={{ background: 'wheat'}} />
+       </div>
       <Main/>
     </div>
   )
